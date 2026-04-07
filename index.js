ab79502b2ba45813ff3aa3fbbd3ec9c267ba51ff
Chong Rui Xin <rxchong1123@gmail.com>
2026-04-08 01:07:11 +0800

    adjusted w frontend

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let express = require("express");
 let path = require("path");
 const cors = require("cors");
 const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, SECRET_KEY } = process.env;
 
 let app = express();
 app.use(cors());
@@ -16,27 +18,94 @@ const pool = new Pool({
   },
 });
 
-app.post("/reservs", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const client = await pool.connect();
   try {
-    const { name, description, date, phone, email, created_at } = req.body;
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
 
+    const userResult = await client.query(
+      "SELECT * FROM r_users WHERE username = $1",
+      [username],
+    );
+
+    if (userResult.rows.length > 0) {
+      return res.status(400).json({ message: "Username already taken" });
+    }
+
+    await client.query(
+      "INSERT INTO r_users (username, password) VALUES ($1, $2)",
+      [username, hashedPassword],
+    );
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.status(500).json({ error: error.message });
+  } finally {
+    client.release();
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const client = await pool.connect();
+  try {
     const result = await client.query(
-      `INSERT INTO reservs (name, description, date, phone, email, created_at) 
-      VALUES ($1, $2, $3, $4, $5, $6) 
-      RETURNING created_at`,
-      [name, description, date, phone, email, created_at],
+      "SELECT * FROM r_users WHERE username = $1",
+      [req.body.username],
     );
 
-    console.log(
-      `Post created successfully with id ${result.rows[0].created_at}`,
-    );
+    const user = result.rows[0];
 
-    res.json({
-      status: "success",
-      data: result.rows[0],
-      message: "Post created successfully",
+    if (!user)
+      return res.status(400).json({ message: "Username or pasword incorrect" });
+
+    const passwordIsValid = await bcrypt.compare(
+      req.body.password,
+      user.password,
+    );
+    if (!passwordIsValid)
+      return res.status(401).json({ auth: false, token: null });
+
+    var token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, {
+      expiresIn: 86400,
     });
+    res.status(200).json({ auth: true, token: token });
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.status(500).json({ error: error.message });
+  } finally {
+    client.release();
+  }
+});
+
+app.post("/reservs", async (req, res) => {
+  const client = await pool.connect();
+  const { name, description, date, phone, email, created_at, user_id } =
+    req.body;
+  try {
+    const userExists = await client.query(
+      "SELECT id FROM r_users WHERE id = $1",
+      [user_id],
+    );
+    if (userExists.rows.length > 0) {
+      const result = await client.query(
+        `INSERT INTO reservs (name, description, date, phone, email, created_at, user_id) 
+      VALUES ($1, $2, $3, $4, $5, $6, $7) 
+      RETURNING *`,
+        [name, description, date, phone, email, created_at, user_id],
+      );
+
+      console.log(`Reservation created successfully with id ${result.rows[0]}`);
+
+      res.json({
+        status: "success",
+        data: result.rows[0],
+        message: "Reservation created successfully",
+      });
+    } else {
+      res.status(400).json({ error: "User does not exist" });
+    }
   } catch (error) {
     console.error("Error: ", error.message);
     res.status(500).json({ error: error.message });
@@ -53,6 +122,28 @@ app.get("/reservs", async (req, res) => {
   } catch (error) {
     console.log(error.stack);
     res.status(500).send("An error occurred");
+  } finally {
+    client.release();
+  }
+});
+
+app.get("/reservs/user/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const client = await pool.connect();
+
+  try {
+    const reservs = await client.query(
+      "SELECT * FROM reservs WHERE user_id = $1",
+      [user_id],
+    );
+    if (reservs.rowCount > 0) {
+      res.json(reservs.rows);
+    } else {
+      res.status(404).json({ error: "No reservations found for this user" });
+    }
+  } catch (error) {
+    console.error("Error", error.message);
+    res.status(500).json({ error: error.message });
   } finally {
     client.release();
   }
@@ -81,8 +172,8 @@ app.put("/reservs", async (req, res) => {
   }
 });
 
-app.delete("/reservs", async (req, res) => {
-  const { created_at } = req.body;
+app.delete("/reservs/:created_at", async (req, res) => {
+  const { created_at } = req.params;
   const client = await pool.connect();
   try {
     await client.query(
